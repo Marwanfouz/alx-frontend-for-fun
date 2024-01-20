c3293338e683c8d91ef6cb46ad7f8d6edcd6d4e9
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-20 18:27:49 +0200

    Hot Fix

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 
 """
-Script that converts markdown to HTML
+Markdown script using python.
 """
+
 import sys
 import os
+import markdown2
 
-if len(sys.argv) < 2:
-    sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+
+def convert_markdown_to_html(Markdown_file, output_file):
+    try:
+        with open(Markdown_file, 'r') as markdown_file:
+            markdown_content = markdown_file.read()
+            html_content = markdown2.markdown(markdown_content)
+            with open(output_file, 'w') as html_file:
+                html_file.write(html_content)
+    except FileNotFoundError:
+        sys.stderr.write(f"Missing {Markdown_file}\n")
+        sys.exit(1)
+
+
+if len(sys.argv) != 3:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
     sys.exit(1)
 
 Markdown_file = sys.argv[1]
 output_file = sys.argv[2]
 
 if not os.path.exists(Markdown_file):
-    sys.stderr.write(f'Missing {Markdown_file}\n')
+    sys.stderr.write(f"Missing {Markdown_file}\n")
     sys.exit(1)
 
+convert_markdown_to_html(Markdown_file, output_file)
 sys.exit(0)
